05b79f8f306d9d10360ce08ed198ca7329608c9d
antonio <antonio109mesquita@gmail.com>
2023-10-06 11:45:32 -0300

    up

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,29 +6,39 @@ import {FiUser, FiMail, FiLock} from 'react-icons/fi'
 import { Link, useNavigate} from 'react-router-dom'
 import { useState } from 'react'
 
-
-export function SignUp(){
-    const [ name,setName] = useState("");
-    const [ email,setEmail] = useState("");
-    const [ password,setPassword] = useState("");
-const navigate = useNavigate();
-   function handleSignUp(){
-   if(!name || !email || !password){
-    return alert("Preencha todos os campos")
-   }
-   api.post("/users" , {name,email,password}).then(() => {
-    alert("Usuario cadastrado com sucesso!")
-    navigate("/");
-   }).catch(error => {
-    if(error.response){
-        alert(error.response.data.message);
+function isValidEmail(email) {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return emailRegex.test(email);
+  }
+  
+export function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+  
+    function handleSignUp() {
+      if (!name || !email || !password) {
+        return alert("Preencha todos os campos");
+      }
+  
+      if (!isValidEmail(email)) {
+        return alert("Endereço de e-mail inválido");
+      }
+  
+      api.post("/users", { name, email, password })
+        .then(() => {
+          alert("Usuário cadastrado com sucesso!");
+          navigate("/");
+        })
+        .catch(error => {
+          if (error.response) {
+            alert(error.response.data.message);
+          } else {
+            alert("Não foi possível cadastrar");
+          }
+        });
     }
-    else{
-        alert("Nao foi possivel cadastrar");
-    }
-   })
-   }
-
 
     return(
 
